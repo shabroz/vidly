5cebc61f7548e70adb9dbb0dbf35722d93b389fa
shabroz <b.shabrozkhan@gmail.com>
2021-08-08 14:43:17 +0100

    sorting implementation

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
-import Like from './common/like';
 import ListGroup from './common/listGroup';
 import Pagination from './common/pagination';
 import { getMovies } from '../services/fakeMovieService';
 import {paginate} from '../utils/paginate';
 import { getGenres } from '../services/fakeGenreService';
+import MoviesTable from './moviesTable';
+import _ from 'lodash';
+import { sort } from 'prelude-ls';
 
 class Movies extends Component {
     state = { 
         movies: [],
         genres: [],
         currentPage: 1,
-        pageSize: 4
+        pageSize: 4,
+        sortColumn:{path:'title', order:'asc'}
      }
     
     componentDidMount(){
-        const genres = [{name:'All Genres' }, ...getGenres()]
+        const genres = [{_id:'', name:'All Genres' }, ...getGenres()]
         this.setState({movies: getMovies(), genres});
     };
     
@@ -40,9 +43,28 @@ class Movies extends Component {
         this.setState({selectedGenre: genre, currentPage: 1});
     };
 
+    handleSort = path => {
+        const sortColumn = {...this.state.sortColumn};
+        if (sortColumn.path === path) 
+        sortColumn.order = (sortColumn.order === 'asc') ? 'desc' : 'asc';
+        else {
+            sortColumn.path =path;
+            sortColumn.order = 'asc';
+        }
+        
+        this.setState({sortColumn});
+    };
+
+
     render() { 
         const { length:count } = this.state.movies;
-        const {pageSize, currentPage,selectedGenre, movies:allMovies} =this.state;
+        const {
+            pageSize, 
+            currentPage,
+            sortColumn,
+            selectedGenre, 
+            movies:allMovies
+        } =this.state;
 
         if (count === 0) return <p>There are no movies in the database.</p> 
         
@@ -50,7 +72,9 @@ class Movies extends Component {
         ? allMovies.filter(m => m.genre._id === selectedGenre._id) 
         : allMovies;
 
-        const movies = paginate(filtered, currentPage, pageSize);
+        const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
+
+        const movies = paginate(sorted, currentPage, pageSize);
 ;        
         return (
             <div className="row">
@@ -63,39 +87,12 @@ class Movies extends Component {
             </div>
             <div className="col">
                 <p>Showing {filtered.length} movies in the database.</p>          
-                <table className="table">
-                <thead>
-                    <tr>
-                        <th>Title</th>
-                        <th>Genre</th>
-                        <th>Stock</th>
-                        <th>Rate</th>
-                        <th/>
-                        <th/>
-                    </tr>
-                </thead>
-            <tbody>
-                {movies.map(movie => ( 
-                <tr key={movie._id}> 
-                    <td>{movie.title}</td>
-                    <td>{movie.genre.name}</td>
-                    <td>{movie.numberInStock}</td>
-                    <td>{movie.dailyRentalRate}</td>
-                    <td>
-                        <Like liked={movie.liked} onClick={()=>this.handleLiked(movie)} />
-                    </td>
-                    <td>
-                        <button
-                            onClick={()=> this.handleDelete(movie)} 
-                            className="btn btn-danger btn-sm"
-                        >
-                            Delete
-                        </button>
-                    </td>
-                </tr>
-                ))}
-            </tbody>
-        </table>
+                <MoviesTable 
+                movies={movies} 
+                onLike={this.hadleLike} 
+                onDelete={this.handleDelete}
+                onSort={this.handleSort}
+                />
         <Pagination 
             itemsCount={filtered.length} 
             pageSize={pageSize}
